fe05ecfec9fbd5e20f89b44f9167a8c3b95e961c
dorianlac <dorian.lacic@gmail.com>
2019-02-17 10:47:01 +0100

    Added nmap2nikto.rb script and readme file

---
+++ README.md
@@ -0,0 +1,14 @@
+nmap2nikto.rb
+===========
+
+Extract http/s links from the nmap.xml file to nikto script.
+
+
+## Requirements
+
+- Ruby
+- Ruby nokogiri
+
+## Example
+
+ruby nmap2nikto.rb nmap.xml
---
+++ nmap2nikto.rb
@@ -0,0 +1,59 @@
+require 'nokogiri'
+
+@doc = Nokogiri::XML(File.open(ARGV[0]));
+
+nmap = @doc.xpath("//host");
+
+puts "#!/bin/sh"
+
+a=1
+nmap.each {|x|
+
+x.xpath('address').each { |host|
+	x.xpath('./ports/port').each { |port|
+			
+	if port['protocol'].eql? "tcp";
+
+		port.xpath('./state').each {|state|
+
+		if state['state'].eql? "open";
+			
+			port.xpath('./service').each {|service|
+			if service['name'].eql? 'http';
+				if service['tunnel'].eql? 'ssl';
+				print "nikto -h https://";
+				print host['addr'];
+				print ":";
+				print port['portid'];
+				print " -C all -o "
+				puts "https_" + host['addr'] + "_" + port['portid']+".txt";
+				
+				else
+				print "nikto -h http://";
+                                print host['addr'];
+                                print ":";
+                                print port['portid'];
+                                print " -C all -o "
+                                puts "http_" + host['addr'] + "_" + port['portid']+".txt";
+				
+			end
+ 
+			elsif service['name'].eql? 'https';
+				print "nikto -h https://";
+                                print host['addr'];
+                                print ":";
+                                print port['portid'];
+                                print " -C all -o "
+                                puts "https_" + host['addr'] + "_" + port['portid']+".txt";
+
+			end
+			}		
+		end
+		}
+	end
+	}
+#end
+}
+
+a=a+1
+}
